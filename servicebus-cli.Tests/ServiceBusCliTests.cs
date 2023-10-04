5a691f15212fd370e9613bfd30173954c65aebcf
wenzzzel <wenzzzel@live.se>
2023-10-04 15:05:29 +0200

    Test: Added file for unit tests for ServiceBusCli main logic

---
+++ servicebus-cli.Tests/ServiceBusCliTests.cs
@@ -0,0 +1,28 @@
+﻿
+namespace servicebus_cli.Tests;
+
+public class ServiceBusCliTests
+{
+    private ServiceBusCli cli;
+    private readonly Mock<IDeadletter> deadletterMock = new Mock<IDeadletter>();
+    private readonly Mock<IHelp> helpMock = new Mock<IHelp>();
+
+    [SetUp]
+    public void Setup()
+    {
+        cli = new ServiceBusCli(deadletterMock.Object, helpMock.Object);
+    }
+
+    [Test]
+    public async Task Run_RunsWithoutCrashing()
+    {
+        //Arrange
+        var args = new string[0];
+
+        //Act
+        await cli.Run(args);
+
+        //Assert
+        Assert.Pass();
+    }
+}
