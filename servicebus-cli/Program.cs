b5c336af91510a5f2695360411d4459007efe96b
wenzzzel <wenzzzel@live.se>
2023-10-04 14:57:12 +0200

    Feature: Abstracted main logic into ServiceBusCli class that can be injected and tested

--- servicebus-cli/Program.cs
+++ servicebus-cli/Program.cs
@@ -8,24 +8,12 @@ public class Program
     public static async Task Main(string[] args)
     {
         var serviceProvider = new ServiceCollection()
+            .AddSingleton<IServiceBusCli, ServiceBusCli>()
             .AddSingleton<IDeadletter, Deadletter>()
             .AddSingleton<IHelp, Help>()
             .BuildServiceProvider();
 
-        var deadletter = serviceProvider.GetService<IDeadletter>();
-        var help = serviceProvider.GetService<IHelp>();
-
-        if (args.Length == 0)
-            help.Run();
-        else
-            switch (args[0])
-            {
-                case "deadletter":
-                    await deadletter.Run(args.Skip(1).ToArray());
-                    break;
-                default:
-                    help.Run();
-                    break;
-            }
+        var serviceBusCli = serviceProvider.GetService<IServiceBusCli>();
+        await serviceBusCli.Run(args);
     }
 }
---
+++ servicebus-cli/ServiceBusCli.cs
@@ -0,0 +1,37 @@
+﻿using servicebus_cli.Subjects;
+
+namespace servicebus_cli;
+
+public interface IServiceBusCli
+{
+    Task Run(string[] args);
+}
+
+public class ServiceBusCli : IServiceBusCli
+{
+    private IDeadletter _deadletter;
+    private IHelp _help;
+
+    public ServiceBusCli(IDeadletter deadletter, IHelp help)
+    {
+
+        _help = help;
+        _deadletter = deadletter;
+    }
+
+    public async Task Run(string[] args)
+    {
+        if (args.Length == 0)
+            _help.Run();
+        else
+            switch (args[0])
+            {
+                case "deadletter":
+                    await _deadletter.Run(args.Skip(1).ToArray());
+                    break;
+                default:
+                    _help.Run();
+                    break;
+            }
+    }
+}
