ec4466c797053b6c5e4eaeb1fbb0b5768cfb458e
wenzzzel <wenzzzel@live.se>
2023-10-06 15:00:41 +0200

    Test: 100% coverage on ServiceBusCli

--- servicebus-cli.Tests/ServiceBusCliTests.cs
+++ servicebus-cli.Tests/ServiceBusCliTests.cs
@@ -14,15 +14,43 @@ public class ServiceBusCliTests
     }
 
     [Test]
-    public async Task Run_RunsWithoutCrashing()
+    public async Task Run_WhenCalledWithZeroArguments_HelpIsCalledOnce()
     {
         //Arrange
-        var args = new string[0];
+        var args = Array.Empty<string>();
 
         //Act
         await cli.Run(args);
 
         //Assert
-        Assert.Pass();
+        helpMock.Verify(x => x.Run(), Times.Once());
+    }
+
+    [Test]
+    public async Task Run_WhenCalledWithBadArgument_HelpIsCalledOnce()
+    {
+        //Arrange
+        var args = new string[] { "This_Argument_Does_Not_Make_Any_Sense" };
+
+        //Act
+        await cli.Run(args);
+
+        //Assert
+        helpMock.Verify(x => x.Run(), Times.Once());
+    }
+
+    [Test]
+    public async Task Run_WhenCalledWithDeadletterArgument_DeadletterIsCalledOnce()
+    {
+        //Arrange
+        var args = new string[] { "deadletter" };
+        var expectedArgument = args.Skip(1).ToArray();
+
+        //Act
+        await cli.Run(args);
+
+        //Assert
+        deadletterMock.Verify(x => x.Run(expectedArgument), Times.Once());
+        helpMock.Verify(x => x.Run(), Times.Never());
     }
 }
