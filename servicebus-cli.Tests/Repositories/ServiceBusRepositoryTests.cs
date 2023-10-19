2dbc8f497db91c0914cbc556b04614d814b07359
wenzzzel <wenzzzel@live.se>
2023-10-19 15:34:05 +0200

    Forgot to remove this temporary test

--- servicebus-cli.Tests/Repositories/ServiceBusRepositoryTests.cs
+++ servicebus-cli.Tests/Repositories/ServiceBusRepositoryTests.cs
@@ -23,16 +23,4 @@ public class ServiceBusRepositoryTests
         //Act + Assert
         Assert.ThrowsAsync<ArgumentException>(() => _repository.ResendDeadletterMessage(fullyQualifiedNamespace, entityPath, useSession));
     }
-
-    [TestCase("emea-grip-ip-async-sbus-prod.servicebus.windows.net")]
-    public async Task ListQueues_TEMPTEST(string fullyQualifiedNamespace)
-    {
-        //Arrange
-
-        //Act 
-        await _repository.ListQueues(fullyQualifiedNamespace);
-
-        //Assert
-        Assert.Pass();
-    }
 }
