6f8a99aec012ad905b637886c7ee95a23f3e5d99
wenzzzel <wenzzzel@live.se>
2026-07-06 11:19:27 +0200

    Added setting for adding aditional columns

--- servicebus-cli/Subjects/Settings/Actions/SettingsActions.cs
+++ servicebus-cli/Subjects/Settings/Actions/SettingsActions.cs
@@ -15,12 +15,17 @@ public class SettingsActions(
     IFileService _fileService,
     IConsoleService _consoleService) : ISettingsActions
 {
+
+    private const string FULLY_QUALIFIED_NAMESPACES = "fullyQualifiedNamespaces";
+    private const string ADDITIONAL_COLUMNS = "additionalColumns";
+    private readonly List<string> _availableSettings = new() { FULLY_QUALIFIED_NAMESPACES, ADDITIONAL_COLUMNS };
+
     public async Task Get(string[] args)
     {
         string selectedSetting = "";
         if (args.Length < 1)
         {
-            selectedSetting = await _consoleService.PromptSelection("Setting: ", new[] { "fullyQualifiedNamespaces" });
+            selectedSetting = await _consoleService.PromptSelection("Setting: ", _availableSettings);
         }
         else
         {
@@ -34,8 +39,11 @@ public class SettingsActions(
 
         switch (selectedSetting)
         {
-            case "fullyQualifiedNamespaces":
+            case FULLY_QUALIFIED_NAMESPACES:
                 await PrintFullyQualifiedNamespaces(userSettings.FullyQualifiedNamespaces);
+                break;
+            case ADDITIONAL_COLUMNS:
+                await PrintAdditionalColumns(userSettings.AdditionalColumns);
                 break;
             default:
                 _helpService.Run();
@@ -51,12 +59,20 @@ public class SettingsActions(
         }
     }
 
+    private async Task PrintAdditionalColumns(Dictionary<string, string> additionalColumns)
+    {
+        foreach (var column in additionalColumns)
+        {
+            _consoleService.WriteMarkup($"[green]{column.Key}[/]: [grey on grey15]{column.Value}[/]");
+        }
+    }
+
     public async Task Set(string[] args)
     {
         string selectedSetting = "";
         if (args.Length < 1)
         {
-            selectedSetting = await _consoleService.PromptSelection("Setting: ", new[] { "fullyQualifiedNamespaces" });
+            selectedSetting = await _consoleService.PromptSelection("Setting: ", _availableSettings);
         }
         else
         {
@@ -69,8 +85,11 @@ public class SettingsActions(
         _consoleService.WriteMarkup($"[grey]Current settings:[/]");
         switch (selectedSetting)
         {
-            case "fullyQualifiedNamespaces":
+            case FULLY_QUALIFIED_NAMESPACES:
                 await SetFullyQualifiedNamespaces(userSettings);
+                break;
+            case ADDITIONAL_COLUMNS:
+                await SetAdditionalColumns(userSettings);
                 break;
             default:
                 _helpService.Run();
@@ -85,6 +104,29 @@ public class SettingsActions(
         var namespaces = newFullyQualifiedNamespaces.Split(',').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToList();
 
         userSettings.FullyQualifiedNamespaces = namespaces;
+
+        var settingsJson = _userSettingsService.Serialize(userSettings);
+
+        _fileService.SetConfigFileContent(settingsJson);
+    }
+
+    private async Task SetAdditionalColumns(UserSettings userSettings)
+    {
+        var newAdditionalColumns = await _consoleService.PromptFreeText("Enter [green]additional column(s)[/] as comma separated values in the following format [grey on grey15]column1Name=path.to.metadata.field,column2Name=path.to.metadata.field[/]:");
+
+        var columns = newAdditionalColumns.Split(',').Select(s => s.Trim()).Where(s => !string.IsNullOrEmpty(s)).ToList();
+
+        var additionalColumns = new Dictionary<string, string>();
+        foreach (var column in columns)
+        {
+            var parts = column.Split('=');
+            if (parts.Length == 2)
+            {
+                additionalColumns[parts[0].Trim()] = parts[1].Trim();
+            }
+        }
+
+        userSettings.AdditionalColumns = additionalColumns;
 
         var settingsJson = _userSettingsService.Serialize(userSettings);
 
